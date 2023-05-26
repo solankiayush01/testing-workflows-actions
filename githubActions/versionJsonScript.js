e49ea5986638ab995c2d1c72b80f7fcc761ab7d5
Ayush Solanki <57408203+solankiayush01@users.noreply.github.com>
2023-05-26 11:45:44 +0530

    Update versionJsonScript.js

--- githubActions/versionJsonScript.js
+++ githubActions/versionJsonScript.js
@@ -129,7 +129,7 @@ async function getDataFromAPI(api) {
         }
         const data = await response.text();
         const dataStr = data.toString();
-        console.log("this is data", data);
+        console.log("this is data", dataStr);
         return data;
     } catch (error) {
         console.error(`Error checking link response code: ${error}`);
